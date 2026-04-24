98fc68c8af3940ce34cbc34c58100b4adbc54b16
yerayduran <yerayduranchaves2006@gmail.com>
2026-04-24 08:36:54 +0200

    Boletin del tema 8

--- src/main/java/boletin1/ejercicio1/Main.java
+++ src/main/java/boletin1/ejercicio1/Main.java
@@ -1,17 +1,36 @@
 package boletin1.ejercicio1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.sql.*;
+import java.util.Properties;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (BufferedReader br = new BufferedReader(new FileReader(Path.of("src/main/resources/classicmodels.properties").toFile()))) {
+            // Cargamos el contenido del properties
+            properties.load(br);
+            // Obtenemos los datos del .properties
+            String conexionString = properties.getProperty("db.url");
+            String usuario = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+            // Creamos la conexión con los datos recogidos anteriormente
+            try (Connection connection = DriverManager.getConnection(conexionString, usuario, password)) {
+                Statement statement = connection.createStatement();
+                // Cargamos los resultados de la consulta
+                ResultSet rs = statement.executeQuery("SELECT * FROM productlines");
+                // Mientras haya filas continúa
+                while (rs.next()) {
+                    // Imprime la línea del producto concreto de esa fila que está repasando
+                    System.out.println(rs.getString("productLine"));
+                }
+            }
+        } catch (InvalidPathException | IOException  | SQLException e) {
+            System.out.println(e);
         }
     }
 }
